9a9f95a4e4a61bc951115d809a4cfa009dbbe3e4
Tristan Deering <tristandeering@hotmail.com>
2018-10-29 23:27:40 +0000

    created function which defines url

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,14 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var url = `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`;
+}
+
+
+
+
+
+
+
