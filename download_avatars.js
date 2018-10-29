cc249a664e76e9151c4817cc52537119f8560579
Tristan Deering <tristandeering@hotmail.com>
2018-10-29 23:59:18 +0000

    added secrets file, logger function, and headers for request

--- download_avatars.js
+++ download_avatars.js
@@ -1,11 +1,31 @@
 var request = require('request');
-
+var token = require('./SECRETS.js')
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb) {
-  var url = `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`;
-}
 
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': token.GITHUB_TOKEN
+    }
+  };
+
+  request(options, function(err, response, body) {
+    cb(err, body);
+  });
+};
+
+var logger = function(error, data) {
+  if (error) {
+    console.log(error)
+  } else {
+    console.log(JSON.parse(data))
+  }
+};
+
+getRepoContributors('jquery', 'jquery', logger)
 
 
 
