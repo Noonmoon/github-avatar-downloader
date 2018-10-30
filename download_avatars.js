164ed5efe2449091141ca0c1a9494887f483edaf
Tristan Deering <tristandeering@hotmail.com>
2018-10-30 04:19:16 +0000

    final adjustements + error message on invalid input

--- download_avatars.js
+++ download_avatars.js
@@ -1,8 +1,14 @@
 var request = require('request');
-var fs = require('fs')
-var token = require('./SECRETS.js')
+var fs = require('fs');
+var token = require('./SECRETS.js');
+var input = process.argv.slice(2);
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+//checks whether or not user has provided valid input, displays error if not
+if (input[0] === undefined || input[1] === undefined) {
+  console.log("Error, please provide valid input: node download_avatars.js <owner> <repo>");
+} else if (input[0] && input[1]) {
 
 //makes a request for json, retrieves array of contributors
 function getRepoContributors(repoOwner, repoName, cb) {
@@ -22,13 +28,12 @@ function getRepoContributors(repoOwner, repoName, cb) {
 //data from getrepocon is passed to be looped through for gathering url and file path
 var logger = function(error, data) {
   if (error) {
-    console.log(error)
-  }
-  var urls = [];
+    console.log(error);
+  };
   for (var objs in data) {
     var item = data[objs];
     downloadImageByURL(item['avatar_url'], item.login);
-  }
+  };
 };
 
 //creates file from specific urls and login given from logger
@@ -44,3 +49,10 @@ function downloadImageByURL(url, filePath) {
          })
          .pipe(fs.createWriteStream(`./avatars/${filePath}.jpg`));
 };
+
+getRepoContributors(input[0], input[1], logger)
+
+};
+
+
+
