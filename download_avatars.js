fe4c3c4f9e15ae5dfab7ef10af2a6cfd1500e3aa
Tristan Deering <tristandeering@hotmail.com>
2018-10-30 01:32:08 +0000

    project complete

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,10 @@
 var request = require('request');
-var fs = require('fs');
-
+var fs = require('fs')
 var token = require('./SECRETS.js')
+
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+//makes a request for json, retrieves array of contributors
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
@@ -12,13 +13,13 @@ function getRepoContributors(repoOwner, repoName, cb) {
       'Authorization': token.GITHUB_TOKEN
     }
   };
-
   request(options, function(err, response, body) {
   var parsd = JSON.parse(body);
   cb(err, parsd);
   });
 };
 
+//data from getrepocon is passed to be looped through for gathering url and file path
 var logger = function(error, data) {
   if (error) {
     console.log(error)
@@ -26,14 +27,11 @@ var logger = function(error, data) {
   var urls = [];
   for (var objs in data) {
     var item = data[objs];
-    urls.push(item['avatar_url'])
+    downloadImageByURL(item['avatar_url'], item.login);
   }
-  console.log(urls)
 };
 
-
-
-
+//creates file from specific urls and login given from logger
 function downloadImageByURL(url, filePath) {
   request.get(url)
          .on('error', function(err) {
@@ -44,12 +42,5 @@ function downloadImageByURL(url, filePath) {
              console.log('Response Status Code: ', response.statusCode);
            };
          })
-         .pipe(fs.createWriteStream(filePath));
-}
-
-
-
-
-
-
-
+         .pipe(fs.createWriteStream(`./avatars/${filePath}.jpg`));
+};
