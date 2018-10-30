5587fa176703ccc0463f2570a719acbaadb6e18c
Tristan Deering <tristandeering@hotmail.com>
2018-10-30 00:37:21 +0000

    created a loop which takes the url of each repo contributor

--- download_avatars.js
+++ download_avatars.js
@@ -2,7 +2,6 @@ var request = require('request');
 var token = require('./SECRETS.js')
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
@@ -11,18 +10,27 @@ function getRepoContributors(repoOwner, repoName, cb) {
       'Authorization': token.GITHUB_TOKEN
     }
   };
-
   request(options, function(err, response, body) {
-    cb(err, body);
+  var parsd = JSON.parse(body);
+  cb(err, parsd);
+
+
+
+
+
   });
 };
 
 var logger = function(error, data) {
   if (error) {
     console.log(error)
-  } else {
-    console.log(JSON.parse(data))
   }
+  var urls = [];
+  for (var objs in data) {
+    var item = data[objs];
+    urls.push(item['avatar_url'])
+  }
+  console.log(urls)
 };
 
 getRepoContributors('jquery', 'jquery', logger)
